2951b4fb9741e0249dbb087c9eea65a5acf5b85f
JoyLand192 <cyw4642@gmail.com>
2020-07-07 17:51:35 +0900

    d

--- index.js
+++ index.js
@@ -72,7 +72,6 @@ client.on('message', ( message ) => {
 client.on('message', (message) => {
     const testEmbed = new Discord.MessageEmbed()
         .setTitle('임베드 테스팅')
-        .setImage(message.author.avatar)
     const pre = message.content.slice(prefix.length).split(" ") // 메세지에서 프리픽스의 글자 수만큼 잘라내고, String.split 메서드를 이용하여 Array로 바꾼다.
     const command = pre.shift().toLowerCase() // Array의 첫번 째 값을 없애고 반환하는 Array.shift 메서드에 String.toLowerCase 메서드로 소문자화 한다.
     const kwriterid = client.users.cache.get('282831530286645258');
